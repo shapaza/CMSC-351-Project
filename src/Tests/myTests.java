759ebbdfc14bd9e1780969727fd8e59bac109d3e
shapaza <wang_brent@yahoo.com>
2015-04-26 20:04:31 -0400

    Fixed minor error with shuffleArray method
    out of bounds exception

--- src/Tests/myTests.java
+++ src/Tests/myTests.java
@@ -41,7 +41,7 @@ public class myTests {
 	public void shuffleArray (int[] arr) {
 		Random rnd = new Random();
 		
-		for (int i = arr.length; i > 0; i--) {
+		for (int i = arr.length - 1; i > 0; i--) {
 			int index = rnd.nextInt(i + 1);
 			
 			int blah = arr[index];
